22b3652bad110cf4e62b5d323fa5b371196b394a
strayacode <dallascuber@gmail.com>
2024-04-14 23:58:41 +1000

    now executing custom linker scripts

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,7 @@ YELLOW='\033[0;33m'
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 RESET='\033[0m'
+DOTFILES=$(pwd -P)
 
 info() {
   printf "${BLUE}$1${RESET}\n"
@@ -27,8 +28,73 @@ print() {
   printf "$1\n"
 }
 
-info "Some info text"
-warn "Some user text"
-success "Some success text"
-print "Print some text"
-fail "Some fail text"
+link() {
+  src=$1
+  dst=$2
+  skip=
+  overwrite=
+
+  print "Attempting to link: $src -> $dst"
+
+  # check if destination already exists in some form
+  if [[ -e "$dst" ]]
+  then
+    warn "Destination already exists: $dst"
+    warn "What would you like to do? [s]kip, [o]verwrite"
+    read -r choice < /dev/tty
+
+    case "$choice" in
+      s )
+        skip='true';; 
+      o )
+        overwrite='true';; 
+      * )
+        ;;
+    esac
+
+    if [[ "$overwrite" == "true" ]]
+    then
+      rm -rf "$dst"
+      success "Overwrote: $dst"
+    fi
+
+    if [[ "$skip" == "true" ]]
+    then
+      print "Skipping: $dst"
+    fi
+  fi
+
+  if [[ "$skip" != "true" ]]
+  then
+    ln -s "$src" "$dst"
+    success "Linked: $dst -> $src"
+  fi
+}
+
+install_dotfiles() {
+  find -H "$DOTFILES" -maxdepth 2 -name 'linker' -not -path '*.git*' | while read linkerfile
+  do
+    print "Found linker file: $linkerfile"
+
+    cat "$linkerfile" | while read -r line
+    do
+      src=$(eval echo "$line" | cut -d '=' -f1)
+      dst=$(eval echo "$line" | cut -d '=' -f2)
+
+      dir=$(dirname "$dst")
+
+      print "Attempting to create directory: $dir"
+      if [ -d "$dir" ]
+      then
+        print "Directory already exists: $dir"
+      else
+        mkdir -p "$dir"
+      fi
+
+      link "$src" "$dst"
+
+    done
+  done
+}
+
+install_dotfiles
